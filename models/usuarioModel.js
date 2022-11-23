7112eff6717ab8d4f3d9c75166ee568b13d85262
Thainara Santiago De Oliveira <thainara.oliveira@vert.com.br>
2022-11-22 21:42:53 -0300

    :art trocando message da password

--- models/usuarioModel.js
+++ models/usuarioModel.js
@@ -19,7 +19,7 @@ const userSchema = new Schema({
             validator: function (value) {
                 return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/.test(value);  //expressao regular (regex)
             },
-            message: "A senha precisa ter caracteres especial e letras maiúsculas"
+            message: "A senha precisa conter caracteres especiais, mínimo 8 letras"
         }
     }
 })
